9650f0ceb1b2ac6c6b995bbcc6a6ec1bc6b27d50
CoRuGi <CR.Github.n6qc4@gmail.com>
2016-06-28 14:56:03 +0200

    feat: Add MyMessageHeardReceiver

---
+++ app/src/main/java/com/androidnanodegree/cr/androidautonotificationsample/MyMessageHeardReceiver.java
@@ -0,0 +1,17 @@
+package com.androidnanodegree.cr.androidautonotificationsample;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MyMessageHeardReceiver extends BroadcastReceiver {
+    public MyMessageHeardReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
