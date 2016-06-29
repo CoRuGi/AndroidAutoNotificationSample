1d8110a7761a6f52d80ca8b6dbcbff1c460339ca
CoRuGi <CR.Github.n6qc4@gmail.com>
2016-06-29 13:18:42 +0200

    feat: Add button to launch notification

--- app/src/main/java/com/androidnanodegree/cr/androidautonotificationsample/MainActivity.java
+++ app/src/main/java/com/androidnanodegree/cr/androidautonotificationsample/MainActivity.java
@@ -2,6 +2,9 @@ package com.androidnanodegree.cr.androidautonotificationsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.androidnanodegree.cr.androidautonotificationsample.util.AndroidAutoNotification;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Launch the Android Auto Notification
+     */
+    public void createAndroidAutoNotification(View view) {
+        AndroidAutoNotification androidAutoNotification = new AndroidAutoNotification(this);
+        androidAutoNotification.show();
     }
 }
