125b26d1622391ab8613c40f1f6c246382b19514
CoRuGi <CR.Github.n6qc4@gmail.com>
2016-06-30 15:15:55 +0200

    feat: Handle a MessageReply broadcast message

--- app/src/main/java/com/androidnanodegree/cr/androidautonotificationsample/MyMessageReplyReceiver.java
+++ app/src/main/java/com/androidnanodegree/cr/androidautonotificationsample/MyMessageReplyReceiver.java
@@ -1,8 +1,13 @@
 package com.androidnanodegree.cr.androidautonotificationsample;
 
+import android.app.RemoteInput;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.androidnanodegree.cr.androidautonotificationsample.util.AndroidAutoNotification;
 
 public class MyMessageReplyReceiver extends BroadcastReceiver {
     public MyMessageReplyReceiver() {
@@ -10,8 +15,18 @@ public class MyMessageReplyReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
+        // First we get the conversationId
+        int conversationId = intent.getIntExtra(AndroidAutoNotification.CONVERSATION_ID, -1);
+
+        // Now we can retrieve the text of the reply and show it in the debbug logging
+        Bundle remoteInput = RemoteInput.getResultsFromIntent(intent);
+        if (null != remoteInput) {
+            CharSequence replyText = remoteInput.getCharSequence(AndroidAutoNotification.VOICE_REPLY_KEY);
+            Log.d(
+                    "AndroidAutoNotification",
+                    "Found voice reply [" + replyText + "] from conversation with id " +
+                            conversationId
+            );
+        }
     }
 }
